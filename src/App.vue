2fe700a44234de69f0412bede8b0ebc1cb632ccf
“erica_torrico” <ericatorrico@gmail.com>
2021-06-05 17:11:19 -0300

    First version of v-data-table

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,24 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <v-container fluid class="pa-7 pa-lg-10">
+        <h1 class="pb-5">Usuarios</h1>
+        <v-data-table
+          v-if="result"
+          :headers="headers"
+          :items="result"
+          :hide-default-footer="true"
+          class="elevation-2"
+        >
+          <template v-if="result" v-slot="result">
+            <tr v-for="item in result" :key="item.id">
+              <td v-for="header in headers" :key="header.value">
+                {{ item[header.value] }}
+              </td>
+            </tr>
+          </template>
+        </v-data-table>
+      </v-container>
     </v-main>
 
     <v-footer padless dark>
@@ -31,18 +48,42 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
+
+import axios from "axios";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    //  HelloWorld,
   },
 
   data: () => ({
-    //
+
+    headers: [
+      {
+        text: 'Nombre',
+        //  align: 'start',
+        //  sortable: false,
+        value: 'name',
+      },
+      { text: 'Usuario', value: 'username' },
+      { text: 'Email', value: 'email' },
+      { text: 'Teléfono', value: 'phone' },
+      { text: 'Website', value: 'website' },
+      { text: 'Nombre empresa', value: 'company.name' },
+      { text: 'Opciones', value: 'options' },
+
+    ],
+    result: null,
   }),
+
+  created () {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((result) => {
+      this.result = result.data;
+    })
+  }
 };
 </script>
 
