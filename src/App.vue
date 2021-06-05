22869ff26a6bff87d18f292c498eb7da2a64620f
“erica_torrico” <ericatorrico@gmail.com>
2021-06-05 14:56:04 -0300

    Customized v-app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app class="bg-gradient">
-      <div class="d-flex align-center">
+    <v-app-bar app prominent class="bg-gradient">
+      <div class="d-flex align-self-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          alt="SFI App Logo"
+          class="mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/sfi_logo.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-avatar size="70" class="align-self-center">
+        <v-img alt="Vuetify Name" src="./assets/avatar.png" contain />
+      </v-avatar>
     </v-app-bar>
 
     <v-main>
