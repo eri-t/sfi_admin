8b85ea618e74a1d2aa6e3bb4f548c14a3aff6543
“erica_torrico” <ericatorrico@gmail.com>
2021-06-06 22:54:56 -0300

    vue router navigation added

---
+++ src/components/userSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-fade-transition mode="out-in">
+      <v-btn @click="$router.go(-1)"> Volver </v-btn>
+    </v-fade-transition>
+
+    <h1>UserId : {{ this.id }}</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "userSection",
+  props: ['userId'],
+
+  data: () => ({
+    id: '',
+  }),
+
+  mounted: function () {
+    this.getId();
+  },
+
+  methods: {
+    getId: function () {
+      this.id = this.$route.params.userId;
+    },
+  },
+
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,56 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import tableSection from "./components/tableSection";
+import userSection from "./components/userSection";
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const listView = {
+  template: `<table-section/>`
+}
+
+const userView = {
+  template: `<user-section :userId="userId"></user-section>`,
+  props: ['userId'],
+}
+
+const routes = [{
+    path: '/home',
+    name: 'Home',
+    component: listView,
+  },
+  {
+    path: '/user/:userId',
+    name: 'User',
+    component: userView,
+  },
+  {
+    path: '*',
+    redirect: '/home'
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+
+  scrollBehavior() {
+    return {
+      x: 0,
+      y: 0
+    };
+  }
+})
+
+Vue.component('table-section', tableSection);
+Vue.component('user-section', userSection);
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
